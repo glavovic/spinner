c3f8a53f3c482ea5e931bad2f0a4afd1b49cb4b9
Armin Glavovic <glavovic.armin@gmail.com>
2022-01-18 19:00:15 +0000

    linted

--- spinner2.js
+++ spinner2.js
@@ -1,11 +1,11 @@
 spinners = ['\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   ', '\n\r']
 
-let x = 100
+let x = 100;
 
 for (let spinner of spinners) {
   setTimeout(() => {
     process.stdout.write(spinner)
-  }, x += 200)
+  }, x += 200);
   
-}
+};
 
